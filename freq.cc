4814118fb1f3360d0b48938220e6e77b57a48a12
Michael Madden <mikemadden42@icloud.com>
2016-05-19 11:30:19 -0500

    Initial versions.

---
+++ freq.cc
@@ -0,0 +1,37 @@
+// Copyright 2016 Michael Madden
+
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <map>
+
+int main(int argc, char const *argv[]) {
+  std::ifstream text(argv[1]);
+  std::map<char, int> freqmap;
+  char c;
+
+  if (argc == 1) {
+    std::cout << "Usage: " << argv[0] << " file" << std::endl;
+    exit(1);
+  }
+
+  if (!text) {
+    std::cout << "Eror opening file: " << argv[1] << std::endl;
+    exit(1);
+  }
+
+  while (text.get(c)) {
+    if (isspace(c) || ispunct(c) || isdigit(c))
+      continue;
+
+    freqmap[tolower(c)]++;
+  }
+
+  for (auto const &ent : freqmap) {
+    std::cout << ent.first << " " << ent.second << std::endl;
+  }
+
+  text.close();
+
+  return 0;
+}
