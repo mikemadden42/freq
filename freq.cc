8315692c4439b5a706d606ffbeeb8013775b9790
Michael Madden <mikemadden42@icloud.com>
2017-09-17 14:23:30 -0500

    Clean up with clang-tidy

--- freq.cc
+++ freq.cc
@@ -6,32 +6,32 @@
 #include <map>
 
 int main(int argc, char const *argv[]) {
-  std::ifstream text(argv[1]);
-  std::map<char, int> freqmap;
-  char c;
+    std::ifstream text(argv[1]);
+    std::map<char, int> freqmap;
+    char c;
 
-  if (argc == 1) {
-    std::cout << "Usage: " << argv[0] << " file" << std::endl;
-    exit(1);
-  }
+    if (argc == 1) {
+        std::cout << "Usage: " << argv[0] << " file" << std::endl;
+        exit(1);
+    }
 
-  if (!text) {
-    std::cout << "Eror opening file: " << argv[1] << std::endl;
-    exit(1);
-  }
+    if (!text) {
+        std::cout << "Eror opening file: " << argv[1] << std::endl;
+        exit(1);
+    }
 
-  while (text.get(c)) {
-    if (isspace(c) || ispunct(c) || isdigit(c))
-      continue;
+    while (text.get(c)) {
+        if (isspace(c) || ispunct(c) || isdigit(c))
+            continue;
 
-    freqmap[tolower(c)]++;
-  }
+        freqmap[tolower(c)]++;
+    }
 
-  for (auto const &ent : freqmap) {
-    std::cout << ent.first << " " << ent.second << std::endl;
-  }
+    for (auto const &ent : freqmap) {
+        std::cout << ent.first << " " << ent.second << std::endl;
+    }
 
-  text.close();
+    text.close();
 
-  return 0;
+    return 0;
 }
